43c64a5a13174be87c18a7e0b744f5e64d01bc5c
Camilo Paternina <cpaterninapertuz95@correo.unicordoba.edu.co>
2023-04-25 09:46:22 -0500

    auth

---
+++ src/app/auth/services/auth.service.ts
@@ -0,0 +1,36 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { env } from 'src/environments/environments';
+import { User } from '../interfaces/user.interface';
+import { Observable, tap } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private baseUrl: string = env.baseUrl;
+  private user?: User;
+
+  constructor(private http: HttpClient) { }
+
+  getCurrentUser(): User | undefined{
+    if(!this.user) return undefined;
+    return structuredClone(this.user);
+  }
+
+  login(email: string, password: string):Observable<User> {
+    return this.http.get<User>(`${this.baseUrl}/users/1`)
+    .pipe(
+      tap(user => {
+        this.user = user;
+        localStorage.setItem('token', user.id.toString());
+      })
+    )
+  }
+
+  logout(){
+    this.user = undefined
+    localStorage.clear()
+  }
+}
